d4da09a5201874148db3a45530dbb53cb10ca883
Hridoy Khan <rkhridoyinfo@gmail.com>
2025-06-02 13:27:22 +0600

    logger and exception file is ready to use

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # from src.wine.logger import logging
 # if __name__ == "__main__":
 #     logging.info('hellow')
+
+# check the exception file is fine
+# from src.wine.exception import WineException
+# import sys
+
+
+# try:
+#     1/0
+# except Exception as e:
+#     raise e
